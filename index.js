f93e544523af4b4c26cb5c9f741fd5bcb3e244fa
wynterwolf <shinyguns@gmail.com>
2020-11-18 17:19:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+
+function moveDodgerLeft() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left);
+    dodger.style.left = `${left -1}px`;
+    }
+
+
+
+function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left);
+    dodger.style.left = `${left + 1}px`;
+    }
